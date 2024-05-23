2e793ca11ad4e8e3f6cd98e8b4a83876eb079b5f
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2024-05-23 19:06:48 -0300

    Adiciona script

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  return res.end("Hello World");
+  return res.end("Hello World from Node.js!");
 });
 
 server.listen(3333);
